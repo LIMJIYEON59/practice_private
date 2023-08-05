af9cc06b7efc44eb13010d1b3f1bcce914f20418
LIMJIYEON59 <chayi2110@gmail.com>
2023-08-05 19:34:05 +0900

    login연습

---
+++ login_practice/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<a href="/join"><h1>회원가입: 절대위치 context path 누락됨 -사용불가</h1></a>
+<a href="./join"><h1>회원가입: 상대위치 엄청 불편함</h1></a>
+<a href="<%request.getContextPath() %>/join00"><h1>회원가입</h1></a>
+</body>
+</html>
--- test20230803/src/main/java/edu/kh/test/user/JdbcTemplate.java
+++ test20230803/src/main/java/edu/kh/test/user/JdbcTemplate.java
@@ -5,14 +5,17 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//The method getConnection() from the type JdbcTemplate is never used locally
+//한번도 쓰인 적이 없어서 뜨는 오류이다.
 public class JdbcTemplate {
-	// 이래야지 광범위하게 new를 안해도되니깐
-	public static Connection getConnection() { // connection 객체 getconnection()이라는 메서드 생성
-		Connection conn = null;
+
+	// getConnection(): 데이터베이스에 연결을 수행하는 메서드이다
+	public static Connection getConnection() { 
+		Connection conn = null;						
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");	// 이걸 로드한다.
 			try {
+				//url,사용자 이름, 비번을 사용하여 DB에 연결한다.
 				conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "abc", "abc");
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -25,14 +28,15 @@ public class JdbcTemplate {
 		}else {
 			System.out.println("연결 실패");
 		}
-		return conn;
+		return conn;	//반환값으로 연결된 Connection 객체를 반환한다.
 	}
 
 	// close: static void = 리턴값이 없으니 이걸 적어줘야 한다.
 	// jdbc에서 사용되는 클래스가 총 3가지 Connection, Statement(원래는 prepared Statement인데 부모 꺼 씀)
 	// ResultSet(close가 필요하면 꼭 써야함)
-
-	// conn.close()해준다 그리고 try-catch 해준다. (이것 때문에 jdbcTemplate)
+	
+	//각각(3개)을 닫는 메서드이다. close(Connection conn)등, 닫을 때 예외가 발생할 수 있으므로 try-catch로 묶어줌
+	
 	// nullpointexception이 많이 뜸 그래서 if문 쓰고 conn이 null이 아닐 때만 close를 해주겠다.
 	public static void close(Connection conn) {
 		try {
@@ -57,10 +61,11 @@ public class JdbcTemplate {
 			e.printStackTrace();
 		}
 	}
-	// close -> try-catch -> null 이 끝났으면 commit도 해준다.
+
 	
-	// setAutoCommit: set으로 시작하는 애들은 return값이 없다. 
-	// boolean으로 하고 autocommit이라고 이름을 만듬
+ 
+	// 지정된 Connection의 자동 커밋 설정을 변경하는 메서드이다. 
+	// Connection 객체의 'setAutoCommit(boolean autoCommit) 메서드를 활용하여 자동 커밋 설정을 변경한다.'
 	public static void setAutoCommit(Connection conn, boolean autocommit) {
 		try {
 			if (conn != null) conn.setAutoCommit(autocommit);	//conn아 autocommit 해줘 그리고 try-catch
@@ -69,7 +74,8 @@ public class JdbcTemplate {
 		}	
 	}
 	
-	// 객체 Connection 
+	// commit(Connection conn) 트랜잭션을 커밋하는 메서드이다. Connection 객체의 'commit()' 메서드를 호출하여 트랜잭션을 커밋한다.
+	// 트랜잭션 처리 후 Connection을 닫는다.
 	public static void commit(Connection conn) {
 		try {
 			if (conn != null) conn.commit();
@@ -77,7 +83,8 @@ public class JdbcTemplate {
 			e.printStackTrace();
 		}
 	}
-	// rollback 을 마무리로 총 5개 적어준다.
+	// rollback(Connection conn) 트랜잭션을 롤백하는 메서드이다. Connection 객체의 'rollback()' 메서드를 호출하여 트랜잭션을 롤백한다.
+	// 트랜잭션 처리 후 Connection을 닫는다.
 	public static void rollback(Connection conn) {	
 		try {
 			if (conn != null) conn.rollback();	//conn을 가지고 와서 rollback을 시도
